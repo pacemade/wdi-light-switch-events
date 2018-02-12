83d67ca7f4605340872d7f535e46fe4c0ecc9eba
Danny Chang <dannychachang@gmail.com>
2018-02-12 13:59:01 -0500

    update main.js to change states when switch is clicked

--- js/main.js
+++ js/main.js
@@ -1,2 +1,17 @@
+var switchEle = document.querySelector('.switch');
+var bodyEle = document.querySelector('body')
+var scriptEle = document.querySelector('.status');
 
+function toggleSwitch() {
+  switchEle.classList.toggle('on');
+  switchEle.classList.toggle('off');
+  bodyEle.classList.toggle('light');
+  bodyEle.classList.toggle('dark');
+  if (scriptEle.innerText === "It's so bright in here!") {
+    scriptEle.innerText = "Who turned off the damn lights!?";
+  } else {
+    scriptEle.innerText = "It's so bright in here!";
+  }
+}
 
+switchEle.addEventListener('click', toggleSwitch);
